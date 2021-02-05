95ada92e0db294b0ca2bb4d0dba4d1251b6b37e0
Emanuel Machado da Silva <emanuel.mch@gmail.com>
2021-02-04 21:52:20 -0300

    [WIP] Initial Commit

---
+++ scripts/integration_test.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+RESULT=0
+TEMP_DIR=$(mktemp -d -t baffl-integration-test-XXXXXXXX)
+
+COMPILER=$1
+TEST_FILE=$2
+TEST_EXECUTABLE=${TEMP_DIR}/executable
+
+TEST_EXPECTED_RESULT=$(cat "${TEST_FILE}.result")
+
+# Compile the test file
+$COMPILER "$TEST_FILE" -o "$TEST_EXECUTABLE"
+$TEST_EXECUTABLE
+TEST_ACTUAL_RESULT=$?
+
+if [ "$TEST_ACTUAL_RESULT" -eq "$TEST_EXPECTED_RESULT" ] ; then
+  echo "Success: $TEST_ACTUAL_RESULT"
+  RESULT=0
+else
+  echo "Failed: Expected $TEST_EXPECTED_RESULT, got $TEST_ACTUAL_RESULT"
+  RESULT=1
+fi
+
+rm -rf $TEMP_DIR
+exit $RESULT
